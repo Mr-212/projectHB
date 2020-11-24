a5f703bbd6cf9aa8ae36400c60285591c7de0af5
Ali Raza <ali.raza@techloyce.com>
2020-11-24 19:05:59 +0500

    updated fix artisan client seeder

--- app/Http/Livewire/OutstandingItems.php
+++ app/Http/Livewire/OutstandingItems.php
@@ -23,7 +23,7 @@ class OutstandingItems extends Component
     {
         $clients = Client::limit(5)->get();
         if(empty($clients) && env('APP_ENV') == 'development'){
-            Artisan::call("db:seed", ['--class' => CreateClientSeeder::class]);
+            Artisan::call("db:seed", ['--class' => 'Database\Seeders\CreateClientSeeder']);
         }
         return view('livewire.outstanding-items',compact('clients'))
             ->extends('layouts.app');
