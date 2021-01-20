eaf36640421a1ae0e8d4dd5d68659a44bc5908e9
Ali Raza <ali.raza@techloyce.com>
2021-01-20 19:50:24 +0500

    updated initla delete component

---
+++ app/Http/Livewire/Component/DeleteItemComponent.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Livewire\Component;
+
+use App\Constants\PropertyStatusConstant;
+use App\Models\Client;
+use App\Models\Property;
+use App\Models\Support\Client\ClientItemCheckListVariables;
+use App\RepoHandlers\ClientPropertyChecklistHandler;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class DeleteItemComponent extends Component
+{
+    public  $client,$property;
+    public  $client_id, $property_id;
+    protected  $client_property_pre_closing_handler = null;
+
+    protected $rules = [
+        'property.sold_price' => 'required|integer',
+        'property.sold_date' => 'required|string',
+        ];
+
+    public function mount($property_id){
+        $this->property_id = $property_id;
+        $this->client_property_pre_closing_handler = new ClientPropertyChecklistHandler(null,$property_id);
+        $this->getClientProperty();
+    }
+
+
+
+    public function hydrate(){
+        $this->client_property_pre_closing_handler = new ClientPropertyChecklistHandler(null,$this->property_id);
+    }
+
+    public function getClientProperty(){
+        $this->property = $this->client_property_pre_closing_handler->getProperty();
+    }
+
+    public function render()
+    {
+        return view('livewire.components.delete-item');
+    }
+
+    public function delete(){
+        $this->validate();
+        try {
+            $this->property->property_status_id = PropertyStatusConstant::HOUSE_SOLD;
+            $this->property->sold_price_entered_by= Auth::id();
+            $this->property->sold_price_entered_at = Carbon::now();
+            if ($this->property->save()) {
+                $this->dispatchBrowserEvent("property-sold-{$this->property_id}", ['error' => false, 'message' => 'House successfully moved to sold section.']);
+                sleep(0.5);
+                return $this->redirect('/house/sold');
+            }
+        }catch (\Throwable $e){
+            report($e);
+        }
+    }
+
+
+
+}
