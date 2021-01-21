d9baaec6b84ad0d46bfcbda7d8f3075ad1ca550d
Ali Raza <ali.raza@techloyce.com>
2021-01-21 19:25:59 +0500

    updated made .

--- routes/web.php
+++ routes/web.php
@@ -61,11 +61,11 @@ Route::middleware(['auth:sanctum', 'verified','before_request'])->group(function
             dd('migration done');
         });
 
-//        Route::get('/do-db-seeder',function (){
-//           //  Artisan::call('db:seed', ['--class' => 'Database\Seeders\UserSeeder']);
-//             Artisan::call('db:seed', ['--class' => 'Database\Seeders\RolePermissionSeeder']);
-//             dd('seeder done');
-//        });
+        Route::get('/do-db-seeder',function (){
+           //  Artisan::call('db:seed', ['--class' => 'Database\Seeders\UserSeeder']);
+             Artisan::call('db:seed', ['--class' => 'Database\Seeders\RolePermissionSeeder']);
+             dd('seeder done');
+        });
     });
 
     Route::get('/dashboard', [DashBoardController::class,'index'])->name('dashboard');
