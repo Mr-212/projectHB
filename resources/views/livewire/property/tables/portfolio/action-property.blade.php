6383a02c178349869e6d58c1a9471acae7ec93ba
Ali Raza <ali.raza@techloyce.com>
2021-01-27 11:33:00 +0500

    fixed portfolio pagination issue added key attr to child compnents

--- resources/views/livewire/property/tables/portfolio/action-property.blade.php
+++ resources/views/livewire/property/tables/portfolio/action-property.blade.php
@@ -1,8 +1,8 @@
 <div class="w-100 mw-100">
     @if(isset($field))
-            @livewire('component.property-repair-request-component',["property_id" => $property_id,"field"=>$field])
+            @livewire('component.property-repair-request-component',["property_id" => $property_id,"field"=>$field],key($property_id))
     @endif
     @if(isset($rent) && isset($key))
-            @livewire('component.edit-input-component',['input_type' =>'number','key' => $key, 'value'=>$rent, "property_id" => $property_id,'object_type'=>'pre_closing'])
+            @livewire('component.edit-input-component',['input_type' =>'number','key' => $key, 'value'=>$rent, "property_id" => $property_id,'object_type'=>'pre_closing'],key($property_id))
     @endif
 </div>
